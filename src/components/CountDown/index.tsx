0aa93b072128cde6c4d0f7ca9015687db2844837
Gabriel Basaglia <garbaroots@hotmail.com>
2025-08-24 01:08:53 +0100

    Criado Context API

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,6 +1,9 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
-import { TimerIcon } from 'lucide-react';
 
 export function Countdown() {
-  return <div className={styles.container}>00:00</div>;
+  const { state } = useTaskContext();
+  return (
+    <div className={styles.container}>{state.formattedSecondsRemaining}</div>
+  );
 }
