bdf27231ab97a9372d5483855d64bbffaeef6226
Gabriel Basaglia <garbaroots@hotmail.com>
2025-09-01 17:01:33 +0100

    React Toastify - adicionando mensagens para o usuário

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
-import { TimerWorkerManager } from '../../workers/timerWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
+import { loadBeep } from '../../utils/loadBeep.ts';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,14 +13,18 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
+
   const worker = TimerWorkerManager.getInstance();
 
   worker.onmessage(e => {
     const countDownSeconds = e.data;
 
-    console.log(countDownSeconds);
-
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
       dispatch({
         type: TaskActionTypes.COMPLETE_TASK,
       });
@@ -34,13 +39,19 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   useEffect(() => {
     if (!state.activeTask) {
-      console.log('Worker terminado por falta de tarefa ativa');
       worker.terminate();
     }
 
     worker.postMessage(state);
   }, [worker, state]);
 
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
       {children}
